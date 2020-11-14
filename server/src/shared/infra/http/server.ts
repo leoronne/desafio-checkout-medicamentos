77631103b163b8689e4e39249bebf29c8414567b
Leonardo Ronne <leoronne@gmail.com>
2020-11-14 11:57:59 -0300

    Changes PORT of the server

--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -29,5 +29,5 @@ app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
   });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT);
